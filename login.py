ea487da5df680310050cfc0f04b3251041f0b0dd
Samyak-gdgps <119036808+Samyak-gdgps@users.noreply.github.com>
2022-11-25 17:29:29 +0530

    Delete login.py

--- login.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector as sql
-conn=sql.connect(host="localhost",user="root",passwd="mysql",database="shop")
-if conn.is_connected():
-    print('\t\tConnection Established')
-    print('----------------------------------------------------------')
-c1 = conn.cursor()
-c1.execute('create table login(name varchar(50),user_id varchar(30)primary key,passwd varchar(20))')
